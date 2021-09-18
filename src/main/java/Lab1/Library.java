75cc9715cb41e9ed1185067e6fdb576bfe88db55
Elaine <ywanglf@connect.ust.hk>
2021-09-18 11:53:49 +0800

    I have made a change

--- src/main/java/Lab1/Library.java
+++ src/main/java/Lab1/Library.java
@@ -6,8 +6,11 @@ package Lab1;
 public class Library {
 	
 	public static void main(String arg[]) {
-		System.out.println("Hello World!");
+		Library lib = new Library();
+		if (lib.someLibraryMethod())
+			System.out.println("Hello World!");
 	}
+	
     public boolean someLibraryMethod() {
         return true;
     }
